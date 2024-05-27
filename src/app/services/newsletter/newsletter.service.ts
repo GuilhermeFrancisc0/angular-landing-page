453986ae45d0e83db4357b1ee18782cd68f37b95
Guilherme Francisco <guilhermefranciscoav@gmail.com>
2024-05-26 21:55:12 -0300

    (feature): Criação do componente newsletter-form

---
+++ src/app/services/newsletter/newsletter.service.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { NewsletterResponse } from '../../types/newsletter/newsletter.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsletterService {
+  private endpointUrl = 'https://faed47pcwb7biktidlecuafuty0aegep.lambda-url.us-east-1.on.aws/';
+
+  constructor(private http: HttpClient) { }
+
+  sendData(name: string, email: string): Observable<NewsletterResponse> {
+    const data = { name, email };
+
+    return this.http.post<NewsletterResponse>(this.endpointUrl, data);
+  }
+}
